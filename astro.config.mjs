8765e19d37352726283fcf72a55abfc9f774d273
Hugo Dorne <hugodorne@gmail.com>
2023-11-02 16:51:00 +0100

    Prettier intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  // GitHub Pages
-  site: 'https://HugoDorne.github.io',
-  base: '/portfolio'
+	integrations: [tailwind()],
+	// GitHub Pages
+	site: "https://HugoDorne.github.io",
+	base: "/portfolio",
 });
